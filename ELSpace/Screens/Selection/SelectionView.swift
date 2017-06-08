2c6d1074883353840772090c277fe91692f6a594
Michał Czerniakowski <mpczerniakowski@gmail.com>
2017-06-08 13:23:28 +0200

    Add New Buttons.

--- ELSpace/Screens/Selection/SelectionView.swift
+++ ELSpace/Screens/Selection/SelectionView.swift
@@ -15,8 +15,42 @@ class SelectionView: UIView {
     
     init() {
         super.init(frame: .zero)
+        configureButtons()
+        configureSubviews()
+        configureAutolayout()
     }
     
     required init?(coder aDecoder: NSCoder) { fatalError("init(coder:) has not been implemented") }
     
+    private func configureButtons() {
+        let hubButtonTitle = "EL Hub"
+        let debateButtonTitle = "EL Debate"
+        
+        hubButton.setTitle(hubButtonTitle, for: .normal)
+        debateButton.setTitle(debateButtonTitle, for: .normal)
+        
+        hubButton.backgroundColor = UIColor("AB9BFF")
+        debateButton.backgroundColor = UIColor("4CC359")
+    }
+    
+    private func configureSubviews() {
+        addSubview(backgroundView)
+        addSubview(hubButton)
+        addSubview(debateButton)
+    }
+    
+    private func configureAutolayout() {
+        let buttonHeight: CGFloat = 50
+        let buttonSpacing: CGFloat = 20
+        
+        backgroundView.edgeAnchors == self.edgeAnchors
+        
+        debateButton.heightAnchor == buttonHeight
+        debateButton.bottomAnchor == self.bottomAnchor - buttonSpacing
+        debateButton.horizontalAnchors == self.horizontalAnchors + buttonSpacing
+        
+        hubButton.heightAnchor == buttonHeight
+        hubButton.bottomAnchor == debateButton.topAnchor - buttonSpacing
+        hubButton.horizontalAnchors == self.horizontalAnchors + buttonSpacing
+    }
 }
--- ELSpace/Screens/Selection/SelectionViewController.swift
+++ ELSpace/Screens/Selection/SelectionViewController.swift
@@ -35,6 +35,5 @@ class SelectionViewController: UIViewController {
     }
     
     func didTapDebateButton(){
-        
     }
 }
