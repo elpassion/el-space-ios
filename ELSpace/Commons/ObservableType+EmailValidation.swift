a0a0798ed26807e4cfbd17d2798dd5bc33b8b1c7
Bartlomiej Guminiak <guminiak.bartlomiej@gmail.com>
2017-06-14 14:22:07 +0200

    Add ObservableType extension

---
+++ ELSpace/Commons/ObservableType+EmailValidation.swift
@@ -0,0 +1,19 @@
+//
+//  Created by Bartlomiej Guminiak on 14/06/2017.
+//  Copyright © 2017 El Passion. All rights reserved.
+//
+
+import GoogleSignIn
+import RxSwift
+
+extension ObservableType where E == GIDGoogleUser {
+    
+    func validate(with validator: EmailValidating, expectedDomain: String) -> Observable<E> {
+        return filter { user in
+            try validator.validate(email: user.profile.email)
+            try validator.validate(email: user.profile.email, with: expectedDomain)
+            return true
+        }
+    }
+    
+}
