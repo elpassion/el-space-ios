e61462415372a43cad3f21efe75334bb14355fbb
Bartlomiej Guminiak <guminiak.bartlomiej@gmail.com>
2017-06-14 14:23:11 +0200

    Update folder structure

---
+++ ELSpaceTests/TestDoubles/GoogleSignInMock.swift
@@ -0,0 +1,23 @@
+//
+//  Created by Bartlomiej Guminiak on 12/06/2017.
+//  Copyright © 2017 El Passion. All rights reserved.
+//
+
+@testable
+import ELSpace
+
+class GoogleSignInMock: GoogleSignInProtocol {
+    
+    weak var delegate: GIDSignInDelegate!
+    
+    weak var uiDelegate: GIDSignInUIDelegate!
+    
+    var signInCalled = false
+    
+    func signIn() {
+        signInCalled = true
+    }
+    
+    var hostedDomain: String!
+    
+}
