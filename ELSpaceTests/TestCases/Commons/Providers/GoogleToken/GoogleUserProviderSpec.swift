9cb2cc546f0e3ae320429a141abca1487560238a
Maciej Gomółka <maciej.gomolka@elpassion.pl>
2017-07-17 12:18:44 +0200

    Remove Trailing whitespaces

--- ELSpaceTests/TestCases/Commons/Providers/GoogleToken/GoogleUserProviderSpec.swift
+++ ELSpaceTests/TestCases/Commons/Providers/GoogleToken/GoogleUserProviderSpec.swift
@@ -12,65 +12,65 @@ import ELSpace
 class GoogleUserProviderSpec: QuickSpec {
     override func spec() {
         context("GoogleUserProvider") {
-            
+
             var sut: GoogleUserProvider!
             var googleSignInMock: GoogleSignInMock!
-            
+
             beforeEach {
                 googleSignInMock = GoogleSignInMock()
                 sut = GoogleUserProvider(googleSignIn: googleSignInMock)
                 sut.configure(with: "abc@gmail.com")
             }
-            
+
             afterEach {
                 googleSignInMock = nil
                 sut = nil
             }
-            
+
             describe("proper hostedDomain") {
-                
+
                 it("should be elpassion.pl") {
                     expect(googleSignInMock.hostedDomain) == "abc@gmail.com"
                 }
-                
+
             }
-            
+
             describe("proper delegate") {
-                
+
                 it("should be sut object") {
                     expect(sut) === googleSignInMock.delegate
                 }
-                
+
             }
 
             describe("signIn method call") {
-                
+
                 beforeEach {
                     let viewController = UIViewController()
                     let _ = sut.signIn(on: viewController)
                 }
-                
+
                 it("should call signIn method on googleSignIn dependency") {
                     expect(googleSignInMock.signInCalled) == true
                 }
-                
+
             }
-            
+
             describe("proper uiDelegate") {
 
                 var viewController: UIViewController!
-                
+
                 beforeEach {
                     viewController = UIViewController()
                     let _ = sut.signIn(on: viewController)
                 }
-                
+
                 it("should set proper uiDelegate") {
                     expect(viewController) === googleSignInMock.uiDelegate
                 }
-                
+
             }
-            
+
         }
     }
 }
