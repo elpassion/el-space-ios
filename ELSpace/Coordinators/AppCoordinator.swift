bdaacefa05e434e7d7d661fbcba6d5e8f63f5ce2
Maciej Gomółka <maciej.gomolka@elpassion.pl>
2017-07-18 15:40:44 +0200

    Refactor AppCoordinator

--- ELSpace/Coordinators/AppCoordinator.swift
+++ ELSpace/Coordinators/AppCoordinator.swift
@@ -9,13 +9,6 @@ import ELDebate
 
 class AppCoordinator {
 
-    fileprivate let window: UIWindow
-    fileprivate let navigationController: UINavigationController
-    fileprivate let loginViewController: LoginViewController
-    fileprivate let selectionViewController: SelectionViewController
-    fileprivate let googleUserProvider: GoogleUserProviding
-    fileprivate let debateRunner: DebateRunning
-
     init(window: UIWindow,
          screenFactory: ScreenFactoring = ScreenFactory(),
          googleUserProvider: GoogleUserProviding = GoogleUserProvider(),
@@ -24,11 +17,11 @@ class AppCoordinator {
         self.loginViewController = screenFactory.loginViewController()
         self.navigationController = screenFactory.navigationController(withRoot: loginViewController)
         self.selectionViewController = screenFactory.selectionViewController()
+        self.screenFactory = screenFactory
         self.debateRunner = debateRunner
         self.googleUserProvider = googleUserProvider
         self.googleUserProvider.configure(with: "elpassion.pl")
-        setupLoginObservable()
-        setupSelectionViewControllerBindings()
+        setupBindings()
     }
 
     func present() {
@@ -36,27 +29,55 @@ class AppCoordinator {
         window.makeKeyAndVisible()
     }
 
+    // MARK: Private
+
+    private let window: UIWindow
+    fileprivate let navigationController: UINavigationController
+    fileprivate let loginViewController: LoginViewController
+    fileprivate let selectionViewController: SelectionViewController
+    fileprivate let googleUserProvider: GoogleUserProviding
+    fileprivate let debateRunner: DebateRunning
+    fileprivate let screenFactory: ScreenFactoring
+
+    // MARK: Bindings
+
+    private func setupBindings() {
+        setupLoginViewControllerBindings()
+        setupSelectionViewControllerBindings()
+    }
+
     fileprivate let disposeBag = DisposeBag()
 
-    private func setupLoginObservable() {
+}
+
+extension AppCoordinator {
+
+    fileprivate func setupLoginViewControllerBindings() {
         loginViewController.loginButtonTap
             .flatMapFirst { [weak self] _ -> Observable<GIDGoogleUser> in
                 guard let `self` = self else { return Observable.empty() }
                 return self.googleUserProvider.signIn(on: self.loginViewController)
-            }.subscribe(onNext: { [weak self] user in
-                print("logged in as \(user.profile.email)")
-                guard let selectionView = self?.selectionViewController else { return }
-                self?.loginViewController.navigationController?.pushViewController(selectionView, animated: true)
-            }, onError: { error in
-                print(error)
+            }.subscribe(onNext: { [weak self] _ in
+                self?.presentSelectionController()
+            }, onError: { [weak self] error in
+                self?.presentError(message: error.localizedDescription)
             }).disposed(by: disposeBag)
+    }
+
+    private func presentSelectionController() {
+        loginViewController.navigationController?.pushViewController(selectionViewController, animated: true)
+    }
+
+    private func presentError(message: String) {
+        let alert = screenFactory.messageAlertController(message: message)
+        loginViewController.navigationController?.pushViewController(alert, animated: true)
     }
 
 }
 
 extension AppCoordinator {
 
-    func setupSelectionViewControllerBindings() {
+    fileprivate func setupSelectionViewControllerBindings() {
         selectionViewController.debateButtonTapObservable
             .subscribe(onNext: { [weak self] in
                 guard let `self` = self else { return }
