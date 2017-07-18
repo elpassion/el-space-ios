6e39f5df6453e6085bc280fcd212415c740118ce
Maciej Gomółka <maciej.gomolka@elpassion.pl>
2017-07-18 08:46:10 +0200

    Refactor SelectionViewController

--- ELSpace/Screens/Selection/SelectionViewController.swift
+++ ELSpace/Screens/Selection/SelectionViewController.swift
@@ -8,13 +8,13 @@ import RxSwift
 
 class SelectionViewController: UIViewController {
 
+    var debateButtonTapObservable: Observable<Void> {
+        return selectionView.debateButton.rx.tap.asObservable()
+    }
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(true, animated: true)
-    }
-
-    var debateButtonTapObservable: Observable<Void> {
-        return selectionView.debateButton.rx.tap.asObservable()
     }
 
     override var preferredStatusBarStyle: UIStatusBarStyle {
@@ -24,6 +24,8 @@ class SelectionViewController: UIViewController {
     override func loadView() {
         view = SelectionView()
     }
+
+    // MARK: Private
     
     private var selectionView: SelectionView {
         guard let selectionView = view as? SelectionView else { fatalError("Expected SelectionView but got \(type(of: view))") }
