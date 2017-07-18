83096417e8c8a36bc7d5e968cf992bc2abfcb644
Maciej Gomółka <maciej.gomolka@elpassion.pl>
2017-07-18 08:45:32 +0200

    Refactor LoginViewController

--- ELSpace/Screens/Login/LoginViewController.swift
+++ ELSpace/Screens/Login/LoginViewController.swift
@@ -10,6 +10,10 @@ import RxSwift
 
 class LoginViewController: UIViewController {
 
+    var loginButtonTap: Observable<Void> {
+        return loginView.loginButton.rx.tap.asObservable()
+    }
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(true, animated: true)
@@ -22,14 +26,12 @@ class LoginViewController: UIViewController {
     override func loadView() {
         view = LoginView()
     }
+
+    // MARK: Private
     
     private var loginView: LoginView {
         guard let loginView = view as? LoginView else { fatalError("Expected LoginView but got \(type(of: view))") }
         return loginView
-    }
-    
-    var loginButtonTap: Observable<Void> {
-        return loginView.loginButton.rx.tap.asObservable()
     }
     
 }
