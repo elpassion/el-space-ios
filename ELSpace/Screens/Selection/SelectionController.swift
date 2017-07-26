d7f7dd927b7530cba98615d468e7bc6f912cd9c4
Maciej Gomółka <maciej.gomolka@elpassion.pl>
2017-07-26 14:46:32 +0200

    Add SelectionController

---
+++ ELSpace/Screens/Selection/SelectionController.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+protocol SelectionControllerSignIn {
+    func signInToHub(with googleIdToken: String,
+                     success: @escaping (_ hubToken: String) -> Void,
+                     failure: @escaping (_ error: Error) -> Void)
+}
+
+class SelectionController: SelectionControllerSignIn {
+
+    init(hubTokenService: HubTokenService, googleIdToken: String) {
+        self.hubTokenService = hubTokenService
+    }
+
+    // MARK: - SelectionControllerLogin
+
+    func signInToHub(with googleIdToken: String,
+                     success: @escaping (_ hubToken: String) -> Void,
+                     failure: @escaping (_ error: Error) -> Void) {
+        _ = hubTokenService.getHubToken(googleTokenId: googleIdToken)
+            .subscribe(onNext: { token in
+                success(token)
+            }, onError: { error in
+                failure(error)
+            })
+    }
+
+    // MARK: - Private
+
+    private let hubTokenService: HubTokenService
+
+}
