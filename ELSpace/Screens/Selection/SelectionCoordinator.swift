dbb7e8514e5080ecb0d9725c16dd4300a693c772
Maciej Gomółka <maciej.gomolka@elpassion.pl>
2017-08-01 17:40:20 +0200

    Add present hub func to SelectionCoordinator

--- ELSpace/Screens/Selection/SelectionCoordinator.swift
+++ ELSpace/Screens/Selection/SelectionCoordinator.swift
@@ -6,10 +6,12 @@ class SelectionCoordinator: Coordinator {
 
     init(debateRunner: DebateRunning,
          viewController: UIViewController,
-         selectionViewController: SelectionViewControlling) {
+         selectionViewController: SelectionViewControlling,
+         activityCoordinatorFactory: ActivityCoordinatorCreation) {
         self.debateRunner = debateRunner
         self.viewController = viewController
         self.selectionViewController = selectionViewController
+        self.activityCoordinatorFactory = activityCoordinatorFactory
         setupBindings()
     }
 
@@ -24,6 +26,7 @@ class SelectionCoordinator: Coordinator {
     private let debateRunner: DebateRunning
     private let viewController: UIViewController
     private let selectionViewController: SelectionViewControlling
+    private let activityCoordinatorFactory: ActivityCoordinatorCreation
 
     // MARK: - Presenting
 
@@ -31,13 +34,22 @@ class SelectionCoordinator: Coordinator {
         guard let navigationController = initialViewController.navigationController else { return }
         debateRunner.start(in: navigationController, applyingDebateStyle: true)
         navigationController.setNavigationBarHidden(false, animated: true)
+        navigationController.navigationBar.setBackgroundImage(nil, for: .default)
+    }
+
+    private func presentHub() {
+        let coordinator = activityCoordinatorFactory.activityCoordinator()
+        let navigationController = initialViewController.navigationController
+        navigationController?.pushViewController(coordinator.initialViewController, animated: true)
+        navigationController?.setNavigationBarHidden(false, animated: true)
     }
 
     // MARK: - Bindings
 
     private func setupBindings() {
         selectionViewController.openHubWithToken
-            .subscribe(onNext: { token in
+            .subscribe(onNext: { [weak self] token in
+                self?.presentHub()
                 print(token)
             }).disposed(by: disposeBag)
 
