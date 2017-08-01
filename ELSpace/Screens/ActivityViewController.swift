05a07dd6bb279e2a39e87f15fe9176a2aff00763
Maciej Gomółka <maciej.gomolka@elpassion.pl>
2017-08-01 17:28:56 +0200

    Add empty ActivityCooridnator and ActivityViewController

---
+++ ELSpace/Screens/ActivityViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class ActivityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        navigationController?.applayHubStyle()
+    }
+
+}
