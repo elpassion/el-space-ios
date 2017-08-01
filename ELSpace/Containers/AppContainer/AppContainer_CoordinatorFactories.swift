478a15da48ba5f39bbea1b967078c5241eb770c9
Maciej Gomółka <maciej.gomolka@elpassion.pl>
2017-08-01 17:39:58 +0200

    Add ActivityCoordinatorCreation protocol

--- ELSpace/Containers/AppContainer/AppContainer_CoordinatorFactories.swift
+++ ELSpace/Containers/AppContainer/AppContainer_CoordinatorFactories.swift
@@ -1,14 +1,28 @@
-extension AppContainer: SelectionCoordinatorCreation {
+extension AppContainer: SelectionCoordinatorCreation,
+                        ActivityCoordinatorCreation {
+
+    // MARK: - SelectionCoordinatorCreation
 
     func selectionCoordinator(googleIdToken: String) -> Coordinator {
         let viewController = selectionViewController(googleIdToken: googleIdToken)
         return SelectionCoordinator(debateRunner: debateRunner,
                                     viewController: viewController,
-                                    selectionViewController: viewController)
+                                    selectionViewController: viewController,
+                                    activityCoordinatorFactory: self)
+    }
+
+    // MARK: - ActivityCoordinatorCreation
+
+    func activityCoordinator() -> ActivityCoordinator {
+        return ActivityCoordinator(viewController: activityViewController())
     }
 
 }
 
 protocol SelectionCoordinatorCreation {
     func selectionCoordinator(googleIdToken: String) -> Coordinator
+}
+
+protocol ActivityCoordinatorCreation {
+    func activityCoordinator() -> ActivityCoordinator
 }
