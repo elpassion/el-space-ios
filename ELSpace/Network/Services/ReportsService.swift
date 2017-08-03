48936b6ac5a529d47c0bd2604c827f7d0bc2934f
Maciej Gomółka <maciej.gomolka@elpassion.pl>
2017-08-03 14:52:26 +0200

    Add ReportsService

---
+++ ELSpace/Network/Services/ReportsService.swift
@@ -0,0 +1,31 @@
+import RxSwift
+import Mapper
+import Alamofire
+
+protocol ReportsServiceProtocol {
+    func getReports(startDate: String?, endDate: String?) -> Observable<[ReportDTO]>
+}
+
+class ReportsService {
+
+    init(apiClient: ApiClientProtocol) {
+        self.apiClient = apiClient
+    }
+
+    func getReports(startDate: String?, endDate: String?) -> Observable<[ReportDTO]> {
+        let params: [String: String] = [
+            "start_date": startDate ?? "",
+            "end_date": endDate ?? ""
+        ]
+        return apiClient.request(path: "activities", method: .get, parameters: params, headers: nil)
+            .map { response -> [ReportDTO] in
+                let jsonArray = response.data.array
+                return try jsonArray.map { try ReportDTO(map: Mapper(JSON: $0 as NSDictionary)) }
+            }
+    }
+
+    // MARK: - ApiClientProtocol
+
+    private let apiClient: ApiClientProtocol
+
+}
