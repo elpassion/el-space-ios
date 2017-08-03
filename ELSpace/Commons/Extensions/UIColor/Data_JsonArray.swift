8979cabe8449db8dedc7330e73b6ce7321d0347b
Maciej Gomółka <maciej.gomolka@elpassion.pl>
2017-08-03 14:52:02 +0200

    Add extension to Data with jsonArray property

---
+++ ELSpace/Commons/Extensions/UIColor/Data_JsonArray.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+extension Data {
+
+    var array: [[String: Any]] {
+        guard let json = try? JSONSerialization.jsonObject(with: self, options: []) else { fatalError("Data not a json") }
+        guard let array = json as? [[String: Any]] else { fatalError("Json not an array") }
+        return array
+    }
+
+}
