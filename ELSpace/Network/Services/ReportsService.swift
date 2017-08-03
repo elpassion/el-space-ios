7f12c200364d3163dcb3461097d6acadd0706db6
Maciej Gomółka <maciej.gomolka@elpassion.pl>
2017-08-03 15:21:34 +0200

    Update ReportsService

--- ELSpace/Network/Services/ReportsService.swift
+++ ELSpace/Network/Services/ReportsService.swift
@@ -3,7 +3,7 @@ import Mapper
 import Alamofire
 
 protocol ReportsServiceProtocol {
-    func getReports(startDate: String?, endDate: String?) -> Observable<[ReportDTO]>
+    func getReports(startDate: String, endDate: String) -> Observable<[ReportDTO]>
 }
 
 class ReportsService: ReportsServiceProtocol {
@@ -14,14 +14,15 @@ class ReportsService: ReportsServiceProtocol {
 
     // MARK: - ReportsServiceProtocol
 
-    func getReports(startDate: String?, endDate: String?) -> Observable<[ReportDTO]> {
+    func getReports(startDate: String, endDate: String) -> Observable<[ReportDTO]> {
         let params: [String: String] = [
-            "start_date": startDate ?? "",
-            "end_date": endDate ?? ""
+            "start_date": startDate,
+            "end_date": endDate
         ]
         return apiClient.request(path: "activities", method: .get, parameters: params, headers: nil)
-            .map { response -> [ReportDTO] in
-                let jsonArray = response.data.jsonArray
+            .map {
+                if let error = ApiError(response: $0) { throw error }
+                let jsonArray = $0.data.jsonArray
                 return try jsonArray.map { try ReportDTO(map: Mapper(JSON: $0 as NSDictionary)) }
             }
     }
