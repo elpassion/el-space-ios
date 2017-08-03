9973958bb9a7c7b41d52b12bca0d93897573abee
Maciej Gomółka <maciej.gomolka@elpassion.pl>
2017-08-03 21:21:04 +0200

    Add test case to jsonArray

---
+++ ELSpaceTests/TestCases/Commons/Extensions/JsonArraySpec.swift
@@ -0,0 +1,116 @@
+import Quick
+import Nimble
+
+@testable import ELSpace
+
+class JsonArraySpec: QuickSpec {
+
+    override func spec() {
+        describe("JsonArray") {
+
+            var jsonData: Data!
+
+            afterEach {
+                jsonData = nil
+            }
+
+            context("when json data is correct") {
+                var result: [[String: AnyObject]]!
+
+                beforeEach {
+                    let array = [
+                        ["fake_key1": "fake_value1"],
+                        ["fake_key2": "fake_value2"]
+                    ]
+                    jsonData = try! JSONSerialization.data(withJSONObject: array, options: [])
+                    result = jsonData.jsonArray
+                }
+
+                describe("result") {
+                    it("should have correct number of elements") {
+                        expect(result).to(haveCount(2))
+                    }
+
+                    describe("first element") {
+                        var dict: [String: AnyObject]!
+
+                        beforeEach {
+                            dict = result.first!
+                        }
+
+                        it("should have correct number of elements") {
+                            expect(dict).to(haveCount(1))
+                        }
+
+                        describe("fake_key1") {
+                            it("should have correct value") {
+                                expect(dict["fake_key1"] as? String).to(equal("fake_value1"))
+                            }
+                        }
+                    }
+
+                    describe("second element") {
+                        var dict: [String: AnyObject]!
+
+                        beforeEach {
+                            dict = result[1]
+                        }
+
+                        it("should have correct number of elements") {
+                            expect(dict).to(haveCount(1))
+                        }
+
+                        describe("fake_key2") {
+                            it("should have correct value") {
+                                expect(dict["fake_key2"] as? String).to(equal("fake_value2"))
+                            }
+                        }
+                    }
+                }
+            }
+
+            context("when json data is incorrect") {
+                var errorFunc: (() throws -> Void)!
+
+                beforeEach {
+                    jsonData = Data()
+                    errorFunc = { _ = jsonData.jsonArray }
+                }
+
+                it("should throw fatalError") {
+                    expect { try? errorFunc() }.to(throwAssertion())
+                }
+            }
+
+            context("when json NOT contain array") {
+                var errorFunc: (() throws -> Void)!
+
+                beforeEach {
+                    let array = [
+                        "fake_key1": "fake_value"
+                    ]
+                    jsonData = try! JSONSerialization.data(withJSONObject: array, options: [])
+                    errorFunc = { _ = jsonData.jsonArray }
+                }
+
+                it("should throw fatalError") {
+                    expect { try? errorFunc() }.to(throwAssertion())
+                }
+            }
+        }
+    }
+
+}
+
+//context("when data is NOT a json") {
+//    var testFunc: (() throws -> Void)!
+//
+//    beforeEach {
+//        jsonData = Data()
+//        testFunc = { _ = jsonData.hubToken }
+//    }
+//
+//    it("should throw fatalError") {
+//        expect { try? testFunc() }.to(throwAssertion())
+//    }
+//}
