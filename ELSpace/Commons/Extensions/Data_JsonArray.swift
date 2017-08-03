a9c07798f2e87d5d43c11e97fe3c3491f3d68818
Maciej Gomółka <maciej.gomolka@elpassion.pl>
2017-08-03 15:21:18 +0200

    Update Data_JsonArray

--- ELSpace/Commons/Extensions/Data_JsonArray.swift
+++ ELSpace/Commons/Extensions/Data_JsonArray.swift
@@ -2,9 +2,9 @@ import Foundation
 
 extension Data {
 
-    var jsonArray: [[String: Any]] {
+    var jsonArray: [[String: AnyObject]] {
         guard let json = try? JSONSerialization.jsonObject(with: self, options: []) else { fatalError("Data not a json") }
-        guard let array = json as? [[String: Any]] else { fatalError("Json not an array") }
+        guard let array = json as? [[String: AnyObject]] else { fatalError("Json not an array") }
         return array
     }
 
