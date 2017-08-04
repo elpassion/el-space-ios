70388cb7e46ee625f6ee8acef4aeb11177ba1289
Maciej Gomółka <maciej.gomolka@elpassion.pl>
2017-08-04 14:48:48 +0200

    Update Date_MonthDates

--- ELSpace/Commons/Extensions/Date_MonthDates.swift
+++ ELSpace/Commons/Extensions/Date_MonthDates.swift
@@ -1,15 +1,14 @@
 import Foundation
+import SwiftDate
 
 extension Date {
 
     func startOfMonth() -> Date {
-        guard let startOfMonth = Calendar.current.date(from: Calendar.current.dateComponents([.year, .month], from: Calendar.current.startOfDay(for: self))) else { return Date() }
-        return startOfMonth
+        return Date().startOf(component: .month) + 1.day
     }
 
     func endOfMonth() -> Date {
-        guard let endOfMonth = Calendar.current.date(byAdding: DateComponents(month: 1, day: -1), to: self.startOfMonth()) else { return Date() }
-        return endOfMonth
+        return Date().endOf(component: .month)
     }
 
 }
