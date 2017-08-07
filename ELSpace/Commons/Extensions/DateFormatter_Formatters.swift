04b597f56f3ac8d6ce6a94ff90901e5555f1787a
Maciej Gomółka <maciej.gomolka@elpassion.pl>
2017-08-07 12:11:20 +0200

    Additional formatter

--- ELSpace/Commons/Extensions/DateFormatter_Formatters.swift
+++ ELSpace/Commons/Extensions/DateFormatter_Formatters.swift
@@ -5,6 +5,14 @@ extension DateFormatter {
     static func shortDateFormatter() -> DateFormatter {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd"
+        dateFormatter.timeZone = TimeZone(identifier: "Europe/Warsaw")
+        return dateFormatter
+    }
+
+    static func dayFormatter() -> DateFormatter {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "d E"
+        dateFormatter.timeZone = TimeZone(identifier: "Europe/Warsaw")
         return dateFormatter
     }
 
