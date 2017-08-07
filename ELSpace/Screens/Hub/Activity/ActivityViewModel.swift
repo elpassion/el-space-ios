8091ae6d0c518df9c0bc89f9176cde5978b50ced
Maciej Gomółka <maciej.gomolka@elpassion.pl>
2017-08-07 12:06:13 +0200

    Update ActivityViewModel
    FetchData, after that create DailyViewModels

--- ELSpace/Screens/Hub/Activity/ActivityViewModel.swift
+++ ELSpace/Screens/Hub/Activity/ActivityViewModel.swift
@@ -1,7 +1,8 @@
 import RxSwift
+import SwiftDate
 
 protocol ActivityViewModelProtocol {
-    func getReports()
+    func getData()
 }
 
 class ActivityViewModel: ActivityViewModelProtocol {
@@ -11,14 +12,25 @@ class ActivityViewModel: ActivityViewModelProtocol {
         setupBindings()
     }
 
-    func getReports() {
+    func getData() {
         activityController.getReports(from: startOfCurrentMonth, to: endOfCurrentMonth)
+        activityController.getProjects()
     }
 
     // MARK: - Private
 
     private let activityController: ActivityControlling
     private let shortDateFormatter = DateFormatter.shortDateFormatter()
+
+    private let projects = Variable<[ProjectDTO]>([])
+    private let reports = Variable<[ReportViewModel]>([])
+    private let viewModels = Variable<[DailyReportViewModel]>([])
+
+    private var days: [Date] {
+        return Date.dates(between: Date().startOf(component: .month),
+                          and: Date().endOf(component: .month),
+                          increment: 1.day)
+    }
 
     private var startOfCurrentMonth: String {
         let date = Date().startOf(component: .month)
@@ -30,12 +42,29 @@ class ActivityViewModel: ActivityViewModelProtocol {
         return shortDateFormatter.string(from: date)
     }
 
+    private func createViewModels() {
+        viewModels.value = days.map { date -> DailyReportViewModel in
+            let reports = self.reports.value.filter { date.isInSameDayOf(date: $0.date) }
+            let viewModel = DailyReportViewModel(date: date, reports: reports, projects: projects.value)
+            return viewModel
+        }
+    }
+
     // MARK: Bindings
 
     private func setupBindings() {
         activityController.reports
-            .subscribe(onNext: { reports in
-                print(reports)
+            .map { $0.map { ReportViewModel(report: $0) } }
+            .bind(to: reports)
+            .disposed(by: disposeBag)
+
+        activityController.projects
+            .bind(to: projects)
+            .disposed(by: disposeBag)
+
+        activityController.didFinishFetch
+            .subscribe(onNext: { [weak self] in
+                self?.createViewModels()
             }).disposed(by: disposeBag)
     }
 
