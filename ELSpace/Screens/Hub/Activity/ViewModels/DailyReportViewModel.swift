1394f8afb84f14a60309eaecbc5feba450c8c1b0
Maciej Gomółka <maciej.gomolka@elpassion.pl>
2017-08-07 13:19:42 +0200

    Add dayType enum and helper func to DailyReportViewModel

--- ELSpace/Screens/Hub/Activity/ViewModels/DailyReportViewModel.swift
+++ ELSpace/Screens/Hub/Activity/ViewModels/DailyReportViewModel.swift
@@ -15,6 +15,22 @@ class DailyReportViewModel {
         return dayFormatter.string(from: date)
     }
 
+    var dayType: DayType {
+        if viewModelsContains(type: .normal) {
+            return DayType.normal
+        } else if date.isInWeekend {
+            return DayType.weekend
+        } else if date.isAfter(date: Date(), granularity: .day) && reportsViewModel.isEmpty {
+            return DayType.missing
+        } else {
+            return DayType.comming
+        }
+    }
+
+    func viewModelsContains(type: ReportType) -> Bool {
+        return reportsViewModel.contains { viewModel -> Bool in viewModel.type == type }
+    }
+
     let reportsViewModel: [ReportDetailsViewModel]
 
     // MARK: - Private
@@ -22,4 +38,11 @@ class DailyReportViewModel {
     private let dayFormatter = DateFormatter.dayFormatter()
     private let date: Date
 
+}
+
+enum DayType {
+    case normal
+    case weekend
+    case missing
+    case comming
 }
