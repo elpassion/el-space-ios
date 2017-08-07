b75205d5cfe45c9327350979102d0afe8fa92b47
Maciej Gomółka <maciej.gomolka@elpassion.pl>
2017-08-07 12:04:09 +0200

    Add extra colors

--- ELSpace/Commons/Extensions/UIColor/UIColor_Color.swift
+++ ELSpace/Commons/Extensions/UIColor/UIColor_Color.swift
@@ -3,6 +3,8 @@ import UIKit
 enum Color: String {
     case purpleCEC1FF = "CEC1FF"
     case purpleAB9BFF = "AB9BFF"
+    case black5F5A6A = "5F5A6A"
+    case grayB3B3B8 = "B3B3B8"
 }
 
 extension UIColor {
