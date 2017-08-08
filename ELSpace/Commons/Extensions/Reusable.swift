1b06d9966fa3e0a538de4986cb3bec0347614a2f
Maciej Gomółka <maciej.gomolka@elpassion.pl>
2017-08-08 11:26:45 +0200

    Refactor Reusable

--- ELSpace/Commons/Extensions/Reusable.swift
+++ ELSpace/Commons/Extensions/Reusable.swift
@@ -5,9 +5,11 @@ protocol Reusable: class {
 }
 
 extension Reusable {
+
     static var reuseIdentifier: String {
         return String(describing: self)
     }
+
 }
 
 extension UITableViewCell: Reusable {}
