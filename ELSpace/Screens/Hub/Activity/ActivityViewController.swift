9da2c55d0c8b9f30111d1ada12cbb6e4e9e4ce89
Maciej Gomółka <maciej.gomolka@elpassion.pl>
2017-08-08 10:53:46 +0200

    Update ActivityViewController
    - Add activity indicator
    - Add ReportsTableViewController as child view controller

--- ELSpace/Screens/Hub/Activity/ActivityViewController.swift
+++ ELSpace/Screens/Hub/Activity/ActivityViewController.swift
@@ -1,11 +1,26 @@
 import UIKit
+import SnapKit
 
 class ActivityViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .white
-        navigationController?.applayHubStyle()
+    init(reportsTableViewController: ReportsTableViewController) {
+        self.reportsTableViewController = reportsTableViewController
+        super.init(nibName: nil, bundle: nil)
+        addChildViewController(reportsTableViewController)
+        view.addSubview(reportsTableViewController.tableView)
+        reportsTableViewController.tableView.snp.makeConstraints { $0.edges.equalTo(0) }
     }
+
+    let reportsTableViewController: ReportsTableViewController
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Loading
+
+    private(set) lazy var loadingIndicator: LoadingIndicator = {
+        return LoadingIndicator(superView: self.view)
+    }()
 
 }
