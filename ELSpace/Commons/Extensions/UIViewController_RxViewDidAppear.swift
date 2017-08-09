43277a6b8614c8f8ea7af262b08a99b1cb84ac35
Maciej Gomółka <maciej.gomolka@elpassion.pl>
2017-08-09 16:38:45 +0200

    Remove unnecessary Rx extension

--- ELSpace/Commons/Extensions/UIViewController_RxViewDidAppear.swift
+++
@@ -1,9 +0,0 @@
-import RxSwift
-
-extension Reactive where Base: UIViewController {
-
-    var viewDidAppear: Observable<Void> {
-        return methodInvoked(#selector(base.viewDidAppear(_:))).map { _ in () }
-    }
-
-}
