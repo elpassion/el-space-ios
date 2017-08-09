b65213619e88727ba9563933668c47461ca9678e
Maciej Gomółka <maciej.gomolka@elpassion.pl>
2017-08-09 16:55:25 +0200

    Add pragma mark in ProjectsServiceStub

--- ELSpaceTests/TestDoubles/Stubs/ProjectsServiceStub.swift
+++ ELSpaceTests/TestDoubles/Stubs/ProjectsServiceStub.swift
@@ -6,6 +6,8 @@ class ProjectsServiceStub: ProjectsServiceProtocol {
 
     var result: [ProjectDTO]!
 
+    // MARK: - ProjectsServiceProtocol
+
     func getProjects() -> Observable<[ProjectDTO]> {
         return result == nil ? Observable.empty() : Observable.just(result!)
     }
