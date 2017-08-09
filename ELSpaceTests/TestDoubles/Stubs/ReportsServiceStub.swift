c0860eeeafa6b18881793c583fde7c9356d22f96
Maciej Gomółka <maciej.gomolka@elpassion.pl>
2017-08-09 16:55:55 +0200

    Add missing pragma mark

--- ELSpaceTests/TestDoubles/Stubs/ReportsServiceStub.swift
+++ ELSpaceTests/TestDoubles/Stubs/ReportsServiceStub.swift
@@ -6,6 +6,8 @@ class ReportsServiceStub: ReportsServiceProtocol {
 
     var result: [ReportDTO]?
 
+    // MARK: - ReportsServiceProtocol
+
     func getReports(startDate: String, endDate: String) -> Observable<[ReportDTO]> {
         return result == nil ? Observable.empty() : Observable.just(result!)
     }
