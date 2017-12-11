49063e3cd5e85aeaf9677803e086ed9a232a8674
Maciej Gomółka <maciej.gomolka@elpassion.pl>
2017-12-11 14:01:29 +0100

    Remove number of sections method

--- ELSpace/Screens/Hub/Activities/ActivitiesViewController.swift
+++ ELSpace/Screens/Hub/Activities/ActivitiesViewController.swift
@@ -60,10 +60,6 @@ class ActivitiesViewController: UITableViewController, ActivitiesViewControlling
 
     // MARK: - UITableViewDataSource
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return viewModels.count
     }
