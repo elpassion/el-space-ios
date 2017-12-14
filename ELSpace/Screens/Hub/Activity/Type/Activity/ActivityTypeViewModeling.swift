7cd702c7c09f7f5cde0ddb4b032abd00cea915c5
Bartlomiej <bartlomiej@Bartlomiejs-MacBook-Pro.local>
2017-12-14 17:36:13 +0100

    Update protocol for single activity type

--- ELSpace/Screens/Hub/Activity/Type/Activity/ActivityTypeViewModeling.swift
+++ ELSpace/Screens/Hub/Activity/Type/Activity/ActivityTypeViewModeling.swift
@@ -1,10 +1,11 @@
 import UIKit
 import RxSwift
 
-protocol ActivityTypeViewModeling {
-    var imageSelected: UIImage { get }
-    var imageUnselected: UIImage { get }
+protocol ActivityTypeViewModeling: class {
+    var type: ActivityType { get }
+    var imageSelected: UIImage? { get }
+    var imageUnselected: UIImage? { get }
     var title: String { get }
     var isSelected: Observable<Bool> { get }
-    var select: AnyObserver<Void> { get }
+    var select: AnyObserver<Bool> { get }
 }
