e6760e649d786e939bd48c1a1cd6fae39f9bdc11
Maciej Gomółka <maciej.gomolka@elpassion.pl>
2017-12-14 16:11:53 +0100

    Remove outdated tests

--- ELSpaceTests/TestCases/ViewControllers/ActivitiesViewControllerSpec.swift
+++ ELSpaceTests/TestCases/ViewControllers/ActivitiesViewControllerSpec.swift
@@ -29,26 +29,6 @@ class ActivitiesViewControllerSpec: QuickSpec {
                 expect { sut = ActivitiesViewController(coder: NSCoder()) }.to(throwAssertion())
             }
 
-            context("when set dataSource") {
-                beforeEach {
-                    sut.viewModels = [
-                        DailyReportViewModel(date: Date(), reports: [], projects: [])
-                    ]
-                }
-
-                it("should have correct numer of sections") {
-                    expect(sut.numberOfSections(in: sut.tableView)).to(equal(1))
-                }
-
-                it("should have correct number of rows in 0 section") {
-                    expect(sut.tableView(sut.tableView, numberOfRowsInSection: 0)).to(equal(1))
-                }
-
-                it("should return correct cell") {
-                    expect(sut.tableView(sut.tableView, cellForRowAt: IndexPath(row: 0, section: 0)) as? ReportCell).toNot(beNil())
-                }
-            }
-
             context("when viewDidAppear") {
                 beforeEach {
                     _ = sut.viewDidAppear.subscribe(viewDidAppearObserver)
