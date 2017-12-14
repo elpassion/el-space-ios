8a203111c2cae124001e26e3da77880efb520858
Maciej Gomółka <maciej.gomolka@elpassion.pl>
2017-12-14 18:14:23 +0100

    Move ActivitiesViewModelStub to separate file

--- ELSpaceTests/TestSnapshot/ActivitiesViewControllerTests.swift
+++ ELSpaceTests/TestSnapshot/ActivitiesViewControllerTests.swift
@@ -140,30 +140,3 @@ class ActivitiesViewControllerTests: FBSnapshotTestCase {
     }
 
 }
-
-class ActivitiesControllingStub: ActivitiesControlling {
-
-    let reportsSubject = PublishSubject<[ReportDTO]>()
-    let projectsSubject = PublishSubject<[ProjectDTO]>()
-    let isLoadingSubject = PublishSubject<Bool>()
-    let didFinishFetchSubject = PublishSubject<Void>()
-
-    var reports: Observable<[ReportDTO]> {
-        return reportsSubject.asObservable()
-    }
-
-    var projects: Observable<[ProjectDTO]> {
-        return projectsSubject.asObservable()
-    }
-
-    var isLoading: Observable<Bool> {
-        return isLoadingSubject.asObservable()
-    }
-    var didFinishFetch: Observable<Void> {
-        return didFinishFetchSubject.asObservable()
-    }
-
-    func getReports(from: String, to: String) {}
-    func getProjects() {}
-
-}
