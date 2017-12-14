f8480ff145ee7da9e8360e6634bb9e8fea147e63
bartguminiak <guminiak.bartlomiej@gmail.com>
2017-12-14 09:57:28 +0100

    Test data

--- ELSpace/Screens/Hub/Activity/Type/Chooser/ChooserActivityTypesViewModel.swift
+++ ELSpace/Screens/Hub/Activity/Type/Chooser/ChooserActivityTypesViewModel.swift
@@ -1,3 +1,11 @@
 class ChooserActivityTypesViewModel: ChooserActivityTypesViewModeling {
 
+    var activityTypeViewModels: [ActivityTypeViewModeling] {
+        let viewModel1 = ActivityTypeViewModel()
+        viewModel1.title = "test 1"
+        let viewModel2 = ActivityTypeViewModel()
+        viewModel2.title = "test 2"
+        return [viewModel1, viewModel2]
+    }
+
 }
