43fb77b51a3803fa5e5ab5d8faa3fa4d9506e69e
bartguminiak <guminiak.bartlomiej@gmail.com>
2017-12-14 22:25:53 +0100

    Add ChooserActivityTypesViewControllerSpec

---
+++ ELSpaceTests/TestCases/ViewControllers/ChooserActivityTypesViewControllerSpec.swift
@@ -0,0 +1,60 @@
+import Quick
+import Nimble
+import RxSwift
+import RxTest
+
+@testable import ELSpace
+
+class ChooserActivityTypesViewControllerSpec: QuickSpec {
+
+    override func spec() {
+        describe("ChooserActivityTypesViewController") {
+            var sut: ChooserActivityTypesViewController!
+            var viewModelMock: ViewModelMock!
+            var scheduler: TestScheduler!
+            var typeSelectedObserver: TestableObserver<ActivityType>!
+
+            beforeEach {
+                scheduler = TestScheduler(initialClock: 0)
+                typeSelectedObserver = scheduler.createObserver(ActivityType.self)
+                viewModelMock = ViewModelMock()
+                sut = ChooserActivityTypesViewController(viewModel: viewModelMock)
+                _ = sut.view
+                _ = sut.selected.subscribe(typeSelectedObserver)
+            }
+
+            context("when try to initialize with init coder") {
+                it("should throw fatalError") {
+                    expect { sut = ChooserActivityTypesViewController(coder: NSCoder()) }.to(throwAssertion())
+                }
+            }
+
+            context("when selecting time report") {
+                beforeEach {
+                    sut.select.onNext(ActivityType.timeReport)
+                }
+
+                it("should select time report") {
+                    expect(typeSelectedObserver.events.last?.value.element).to(equal(ActivityType.timeReport))
+                }
+
+                context("when selecting vacation") {
+                    beforeEach {
+                        viewModelMock.activityTypeViewModels[1].isSelected.onNext(true)
+                    }
+
+                    it("should select vacation") {
+                        expect(typeSelectedObserver.events.last?.value.element).to(equal(ActivityType.vacation))
+                    }
+                }
+            }
+        }
+    }
+}
+
+private struct ViewModelMock: ChooserActivityTypesViewModeling {
+
+    var activityTypeViewModels: [ActivityTypeViewModeling] = [ActivityTypeViewModel(type: .timeReport),
+                                                              ActivityTypeViewModel(type: .vacation)]
+
+}
