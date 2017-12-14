12371aa3cadfae5d061a67458b2bbacf08c95278
Maciej Gomółka <maciej.gomolka@elpassion.pl>
2017-12-14 18:11:53 +0100

    Add ActivitiesViewController snapshot tests

---
+++ ELSpaceTests/TestSnapshot/ActivitiesViewControllerTests.swift
@@ -0,0 +1,169 @@
+import FBSnapshotTestCase
+import RxSwift
+
+@testable import ELSpace
+
+class ActivitiesViewControllerTests: FBSnapshotTestCase {
+
+    var sut: ActivitiesViewController!
+    var coordinator: ActivitiesCoordinator!
+    var viewModel: ActivitiesViewModel!
+    var activitiesControllingStub: ActivitiesControllingStub!
+
+    override func setUp() {
+        super.setUp()
+        recordMode = false
+        activitiesControllingStub = ActivitiesControllingStub()
+        viewModel = ActivitiesViewModel(activitiesController: activitiesControllingStub,
+                                        todayDate: dateFormatter.date(from: "2017-12-05")!)
+        sut = ActivitiesViewController()
+        coordinator = ActivitiesCoordinator(viewController: sut,
+                                            activitiesViewController: sut,
+                                            viewModel: viewModel)
+        isDeviceAgnostic = true
+        sut.view.frame = UIScreen.main.bounds
+    }
+
+    override func tearDown() {
+        super.tearDown()
+        sut = nil
+        coordinator = nil
+        viewModel = nil
+        activitiesControllingStub = nil
+    }
+
+    func testNormalReport() {
+        activitiesControllingStub.projectsSubject.onNext(projects)
+        activitiesControllingStub.reportsSubject.onNext([
+            firstNormalReport,
+            secondNormalReport,
+            paidVacationReport,
+            unpaidVacationReport,
+            sickLeaveReport,
+            thirdNormalReport])
+        activitiesControllingStub.didFinishFetchSubject.onNext(())
+        verifyView()
+    }
+
+    // MARK: - Helpers
+
+    private func verifyView() {
+        FBSnapshotVerifyView(sut.view)
+        expectation(description: "Should display correct view").fulfill()
+        waitForExpectations(timeout: 1.0)
+    }
+
+    private lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        return formatter
+    }()
+
+    // MARK: - Fake data source
+
+    // MARK: - Reports
+
+    private var firstNormalReport: ReportDTO {
+        return defaultReportDTO()
+    }
+
+    private var secondNormalReport: ReportDTO {
+        return defaultReportDTO(projectId: 2, value: "8.0")
+    }
+
+    private var thirdNormalReport: ReportDTO {
+        return defaultReportDTO(projectId: 3, value: "5.0", performedAt: "2017-12-03")
+    }
+
+    private var paidVacationReport: ReportDTO {
+        return defaultReportDTO(projectId: nil, performedAt: "2017-12-05", comment: nil, reportType: 1)
+    }
+
+    private var unpaidVacationReport: ReportDTO {
+        return defaultReportDTO(projectId: nil, performedAt: "2017-12-06", comment: nil, reportType: 2)
+    }
+
+    private var sickLeaveReport: ReportDTO {
+        return defaultReportDTO(projectId: nil, performedAt: "2017-12-07", comment: nil, reportType: 3)
+    }
+
+    private func defaultReportDTO(id: Int = 1,
+                                  userId: Int = 1,
+                                  projectId: Int? = 1,
+                                  value: String = "8.0",
+                                  performedAt: String = "2017-12-01",
+                                  comment: String? = "fake_comment",
+                                  createdAt: String = "fake_created_at",
+                                  updatedAt: String = "fake_updated_at",
+                                  billable: Bool = true,
+                                  reportType: Int = 0) -> ReportDTO {
+        return ReportDTO(id: id,
+                         userId: userId,
+                         projectId: projectId,
+                         value: value,
+                         performedAt: performedAt,
+                         comment: comment,
+                         createdAt: createdAt,
+                         updatedAt: updatedAt,
+                         billable: billable,
+                         reportType: reportType)
+    }
+
+    // MARK: - Projects
+
+    private var projects: [ProjectDTO] {
+        return [firstProjectDTO,
+                secondProjectDTO,
+                thirdProjectDTO,
+                fourthProjectDTO,
+                fifthProjectDTO]
+    }
+
+    private var firstProjectDTO: ProjectDTO {
+        return ProjectDTO(name: "Project id: 1", id: 1)
+    }
+
+    private var secondProjectDTO: ProjectDTO {
+        return ProjectDTO(name: "Project id: 2", id: 2)
+    }
+
+    private var thirdProjectDTO: ProjectDTO {
+        return ProjectDTO(name: "Project id: 3", id: 3)
+    }
+
+    private var fourthProjectDTO: ProjectDTO {
+        return ProjectDTO(name: "Project id: 4", id: 4)
+    }
+
+    private var fifthProjectDTO: ProjectDTO {
+        return ProjectDTO(name: "Project id: 5", id: 5)
+    }
+
+}
+
+class ActivitiesControllingStub: ActivitiesControlling {
+
+    let reportsSubject = PublishSubject<[ReportDTO]>()
+    let projectsSubject = PublishSubject<[ProjectDTO]>()
+    let isLoadingSubject = PublishSubject<Bool>()
+    let didFinishFetchSubject = PublishSubject<Void>()
+
+    var reports: Observable<[ReportDTO]> {
+        return reportsSubject.asObservable()
+    }
+
+    var projects: Observable<[ProjectDTO]> {
+        return projectsSubject.asObservable()
+    }
+
+    var isLoading: Observable<Bool> {
+        return isLoadingSubject.asObservable()
+    }
+    var didFinishFetch: Observable<Void> {
+        return didFinishFetchSubject.asObservable()
+    }
+
+    func getReports(from: String, to: String) {}
+    func getProjects() {}
+
+}
