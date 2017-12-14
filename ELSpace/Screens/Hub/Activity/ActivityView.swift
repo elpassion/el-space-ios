185040515cfbd812220c70bda93ce684bdf4dd34
bartguminiak <guminiak.bartlomiej@gmail.com>
2017-12-14 09:55:55 +0100

    Add ActivityView

---
+++ ELSpace/Screens/Hub/Activity/ActivityView.swift
@@ -0,0 +1,40 @@
+import UIKit
+import Anchorage
+
+class ActivityView: UIView {
+
+    init() {
+        super.init(frame: .zero)
+        addSubviews()
+        setupLayout()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    var activityTypeView: UIView? {
+        didSet {
+            if let view = activityTypeView {
+                activityTypeContainer.addSubview(view)
+                view.edgeAnchors == activityTypeContainer.edgeAnchors
+            }
+        }
+    }
+
+    // MARK: Private
+
+    private let activityTypeContainer = UIView(frame: .zero)
+
+    private func addSubviews() {
+        addSubview(activityTypeContainer)
+    }
+
+    private func setupLayout() {
+        activityTypeContainer.topAnchor == topAnchor + 16
+        activityTypeContainer.leadingAnchor == leadingAnchor + 16
+        activityTypeContainer.trailingAnchor == trailingAnchor - 16
+        activityTypeContainer.heightAnchor == 64
+    }
+
+}
