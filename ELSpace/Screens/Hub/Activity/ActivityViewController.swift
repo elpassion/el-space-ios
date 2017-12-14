985fd40ac8d5e1e3edfcc4f16cfc8af15a1c5908
bartguminiak <guminiak.bartlomiej@gmail.com>
2017-12-14 09:56:20 +0100

    Add activity types to activity screen

--- ELSpace/Screens/Hub/Activity/ActivityViewController.swift
+++ ELSpace/Screens/Hub/Activity/ActivityViewController.swift
@@ -11,13 +11,27 @@ class ActivityViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
 
+    override func loadView() {
+        view = ActivityView()
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = "New activity"
         navigationItem.rightBarButtonItem = addBarButton
+
+        let activityTypeViewController = ChooserActivityTypeAssembly().viewController()
+        activityTypeViewController.view.backgroundColor = .red
+        addChildViewController(activityTypeViewController)
+        activityTypeViewController.didMove(toParentViewController: self)
+        activityView.activityTypeView = activityTypeViewController.view
     }
 
     // MARK: - Private
+
+    private var activityView: ActivityView! {
+        return view as? ActivityView
+    }
 
     private let addBarButton = UIBarButtonItem(title: "Add", style: .plain, target: nil, action: nil)
 
