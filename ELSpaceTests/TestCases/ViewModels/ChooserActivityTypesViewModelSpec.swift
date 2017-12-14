c8afcc67f0ae2074180c496eab8002b43255326f
bartguminiak <guminiak.bartlomiej@gmail.com>
2017-12-14 21:48:27 +0100

    Add tests for logic

--- ELSpaceTests/TestCases/ViewModels/ChooserActivityTypesViewModelSpec.swift
+++ ELSpaceTests/TestCases/ViewModels/ChooserActivityTypesViewModelSpec.swift
@@ -1,5 +1,6 @@
 import Quick
 import Nimble
+import RxTest
 
 @testable import ELSpace
 
@@ -41,6 +42,57 @@ class ChooserActivityTypesViewModelSpec: QuickSpec {
                 it("should have fifth time report") {
                     let viewModel = sut.activityTypeViewModels[4] as! ActivityTypeViewModel
                     expect(viewModel).to(equal(ActivityTypeViewModel(type: .conference)))
+                }
+            }
+
+            describe("switching types") {
+                var scheduler: TestScheduler!
+                var isTimeReportSelectedObserver: TestableObserver<Bool>!
+                var isVacationSelectedObserver: TestableObserver<Bool>!
+                var isDayOffSelectedObserver: TestableObserver<Bool>!
+                var isSickLeaveSelectedObserver: TestableObserver<Bool>!
+                var isConferenceSelectedObserver: TestableObserver<Bool>!
+
+                beforeEach {
+                    scheduler = TestScheduler(initialClock: 0)
+                    isTimeReportSelectedObserver = scheduler.createObserver(Bool.self)
+                    isVacationSelectedObserver = scheduler.createObserver(Bool.self)
+                    isDayOffSelectedObserver = scheduler.createObserver(Bool.self)
+                    isSickLeaveSelectedObserver = scheduler.createObserver(Bool.self)
+                    isConferenceSelectedObserver = scheduler.createObserver(Bool.self)
+                    _ = sut.activityTypeViewModels[0].isSelected.asObservable().subscribe(isTimeReportSelectedObserver)
+                    _ = sut.activityTypeViewModels[1].isSelected.asObservable().subscribe(isVacationSelectedObserver)
+                    _ = sut.activityTypeViewModels[2].isSelected.asObservable().subscribe(isDayOffSelectedObserver)
+                    _ = sut.activityTypeViewModels[3].isSelected.asObservable().subscribe(isSickLeaveSelectedObserver)
+                    _ = sut.activityTypeViewModels[4].isSelected.asObservable().subscribe(isConferenceSelectedObserver)
+                }
+
+                context("when selecting time report") {
+                    beforeEach {
+                        sut.activityTypeViewModels[0].isSelected.onNext(true)
+                    }
+
+                    it("should properly update state") {
+                        expect(isTimeReportSelectedObserver.events.last?.value.element).to(beTrue())
+                        expect(isVacationSelectedObserver.events.last).to(beNil())
+                        expect(isDayOffSelectedObserver.events.last).to(beNil())
+                        expect(isSickLeaveSelectedObserver.events.last).to(beNil())
+                        expect(isConferenceSelectedObserver.events.last).to(beNil())
+                    }
+
+                    context("when selecting vacation") {
+                        beforeEach {
+                            sut.activityTypeViewModels[1].isSelected.onNext(true)
+                        }
+
+                        it("should properly update state") {
+                            expect(isTimeReportSelectedObserver.events.last?.value.element).to(beFalse())
+                            expect(isVacationSelectedObserver.events.last?.value.element).to(beTrue())
+                            expect(isDayOffSelectedObserver.events.last).to(beNil())
+                            expect(isSickLeaveSelectedObserver.events.last).to(beNil())
+                            expect(isConferenceSelectedObserver.events.last).to(beNil())
+                        }
+                    }
                 }
             }
         }
