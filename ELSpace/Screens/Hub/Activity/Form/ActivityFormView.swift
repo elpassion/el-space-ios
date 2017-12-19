d5e1c5a3c5a45e4b3aeabdd057c1f6d5cdf6b98f
Bartlomiej <bartlomiej@Bartlomiejs-MacBook-Pro.local>
2017-12-19 18:06:43 +0100

    Add ActivityFormView

---
+++ ELSpace/Screens/Hub/Activity/Form/ActivityFormView.swift
@@ -0,0 +1,52 @@
+import UIKit
+import Anchorage
+
+class ActivityFormView: UIView {
+
+    init() {
+        super.init(frame: .zero)
+        addSubviews()
+        setupLayout()
+        backgroundColor = .white
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    let dateTextView = TextView(title: "PERFORMED AT")
+    let projectTextView = TextView(title: "PROJECT")
+    let hoursTextView = TextView(title: "HOURS")
+    let commentTextView = TextView(title: "COMMENT")
+
+    // MARK: - Privates
+
+    private lazy var stackView = Factory.stackView(views: [self.dateTextView,
+                                                   self.projectTextView,
+                                                   self.hoursTextView,
+                                                   self.commentTextView,
+                                                   UIView(frame: .zero)])
+
+    private func addSubviews() {
+        addSubview(stackView)
+    }
+
+    private func setupLayout() {
+        stackView.edgeAnchors == edgeAnchors + 16
+    }
+
+}
+
+private extension ActivityFormView {
+    struct Factory {
+
+        static func stackView(views: [UIView]) -> UIStackView {
+            let view = UIStackView(arrangedSubviews: views)
+            view.axis = .vertical
+            view.distribution = .fill
+            view.spacing = 6
+            return view
+        }
+
+    }
+}
