a139c7b38795ebd416368f582e2b3510b84858bc
Dariusz Rybicki <darrarski@gmail.com>
2017-12-22 12:48:38 +0100

    Add addAction observer to ActivityViewModelProtocol

--- ELSpace/Screens/Hub/Activity/ViewModels/ActivityViewModel.swift
+++ ELSpace/Screens/Hub/Activity/ViewModels/ActivityViewModel.swift
@@ -1,7 +1,15 @@
-protocol ActivityViewModelProtocol {
+import RxSwift
 
+protocol ActivityViewModelProtocol {
+    var addAction: AnyObserver<Void> { get }
 }
 
 class ActivityViewModel: ActivityViewModelProtocol {
+
+    // MARK: ActivityViewModelProtocol
+
+    var addAction: AnyObserver<Void> {
+        return AnyObserver(onNext: { print("ADD action not implemented") })
+    }
 
 }
