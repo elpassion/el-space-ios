f8558120244e8936358ba0f65600a3d3a6b13b55
Maciej Gomółka <maciej.gomolka@elpassion.pl>
2017-12-27 10:41:29 +0100

    Update constraints in ReportView

--- ELSpace/Screens/Hub/Activities/View/ReportView.swift
+++ ELSpace/Screens/Hub/Activities/View/ReportView.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Anchorage
 
 class ReportView: UIView {
 
@@ -53,59 +54,56 @@ class ReportView: UIView {
     // MARK: - Layout
 
     private func setupLayout() {
-        contentContainer.snp.makeConstraints {
-            $0.top.bottom.equalTo(0)
-            $0.right.equalTo(-20)
-            $0.left.equalTo(20)
-        }
-        titleLabel.snp.makeConstraints {
-            $0.left.equalTo(rightStripeView.snp.right).offset(80)
-            $0.top.equalTo(17)
-            $0.right.lessThanOrEqualTo(addIconView.snp.left).offset(-10)
-            $0.bottom.lessThanOrEqualTo(-17)
-        }
-        dateLabel.snp.makeConstraints {
-            $0.left.equalTo(rightStripeView.snp.right).offset(17)
-            $0.right.lessThanOrEqualTo(titleLabel.snp.left).offset(-3)
-            $0.top.equalTo(17)
-            $0.bottom.lessThanOrEqualTo(separatorView.snp.top).offset(-17)
-        }
-        reportDetailsContainer.snp.makeConstraints {
-            $0.top.equalTo(titleLabel.snp.bottom).offset(15)
-            $0.left.equalTo(titleLabel.snp.left)
-            $0.right.equalTo(0)
-            $0.bottom.equalTo(0)
-        }
-        rightStripeView.snp.makeConstraints {
-            $0.width.equalTo(3)
-            $0.top.left.bottom.equalTo(0)
-        }
-        addIconView.snp.makeConstraints {
-            $0.height.width.equalTo(19)
-            $0.right.equalTo(-20)
-            $0.top.equalTo(17)
-        }
-        separatorView.snp.makeConstraints {
-            $0.height.equalTo(0.5)
-            $0.left.equalTo(20)
-            $0.right.equalTo(-20)
-            $0.bottom.equalTo(0)
-        }
+        contentContainer.topAnchor == topAnchor
+        contentContainer.bottomAnchor == bottomAnchor
+        contentContainer.rightAnchor == rightAnchor - 20
+        contentContainer.leftAnchor == leftAnchor + 20
+
+        titleLabel.leftAnchor == rightStripeView.rightAnchor + 80
+        titleLabel.topAnchor == contentContainer.topAnchor + 17
+        titleLabel.rightAnchor <= addIconView.leftAnchor - 10
+        titleLabel.bottomAnchor <= contentContainer.bottomAnchor - 17
+
+        dateLabel.leftAnchor == rightStripeView.rightAnchor + 17
+        dateLabel.rightAnchor <= titleLabel.leftAnchor - 3
+        dateLabel.topAnchor == contentContainer.topAnchor + 17
+        dateLabel.bottomAnchor <= separatorView.topAnchor - 17
+
+        reportDetailsContainer.topAnchor == titleLabel.bottomAnchor + 15
+        reportDetailsContainer.leftAnchor == titleLabel.leftAnchor
+        reportDetailsContainer.rightAnchor == contentContainer.rightAnchor
+        reportDetailsContainer.bottomAnchor == contentContainer.bottomAnchor
+
+        rightStripeView.widthAnchor == 3
+        rightStripeView.topAnchor == contentContainer.topAnchor
+        rightStripeView.leftAnchor == contentContainer.leftAnchor
+        rightStripeView.bottomAnchor == contentContainer.bottomAnchor
+
+        addIconView.heightAnchor == 19
+        addIconView.widthAnchor == 19
+        addIconView.rightAnchor == contentContainer.rightAnchor - 20
+        addIconView.topAnchor == contentContainer.topAnchor + 17
+
+        separatorView.heightAnchor == 0.5
+        separatorView.leftAnchor == contentContainer.leftAnchor + 20
+        separatorView.rightAnchor == contentContainer.rightAnchor - 20
+        separatorView.bottomAnchor == contentContainer.bottomAnchor
     }
 
     private func setupReportDetailsLayout() {
         reportDetailsViews.enumerated().forEach { index, view in
             let isFirst = reportDetailsViews.first == view
             let isLast = reportDetailsViews.last == view
-            view.snp.makeConstraints {
-                if isFirst {
-                    $0.top.equalTo(0)
-                } else {
-                    let previous = reportDetailsViews[index - 1]
-                    $0.top.equalTo(previous.snp.bottom).offset(10)
-                }
-                $0.right.left.equalTo(0)
-                if isLast { $0.bottom.equalTo(-19) }
+            if isFirst {
+                view.topAnchor == reportDetailsContainer.topAnchor
+            } else {
+                let previous = reportDetailsViews[index - 1]
+                view.topAnchor == previous.bottomAnchor + 10
+            }
+            view.rightAnchor == reportDetailsContainer.rightAnchor
+            view.leftAnchor == reportDetailsContainer.leftAnchor
+            if isLast {
+                view.bottomAnchor == reportDetailsContainer.bottomAnchor - 19
             }
         }
     }
