5a3435a95926c2e93f571571cca42adcf7f535b7
Maciej Gomółka <mgomolka92@gmail.com>
2018-05-24 11:01:18 +0200

    Hide debate button

--- ELSpace/Screens/Selection/SelectionView.swift
+++ ELSpace/Screens/Selection/SelectionView.swift
@@ -17,6 +17,7 @@ class SelectionView: UIView {
         configureButtons()
         configureSubviews()
         configureAutolayout()
+        debateButton.isHidden = true
     }
 
     required init?(coder aDecoder: NSCoder) { fatalError("init(coder:) has not been implemented") }
