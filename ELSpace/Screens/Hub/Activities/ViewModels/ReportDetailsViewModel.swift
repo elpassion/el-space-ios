00cd18222c715f9f18e723b808ad12fcea55569c
Maciej Gomółka <maciej.gomolka@elpassion.pl>
2018-06-07 09:48:31 +0200

    Display day reported as conference

--- ELSpace/Screens/Hub/Activities/ViewModels/ReportDetailsViewModel.swift
+++ ELSpace/Screens/Hub/Activities/ViewModels/ReportDetailsViewModel.swift
@@ -34,6 +34,7 @@ class ReportDetailsViewModel: ReportDetailsViewModelProtocol {
         case .some(.paidVacations): return weekdaysHoursOfWork
         case .some(.unpaidDayOff): return weekdaysHoursOfWork
         case .some(.sickLeave): return weekdaysHoursOfWork
+        case .some(.conference): return weekdaysHoursOfWork
         default: return 0.0
         }
     }
@@ -66,4 +67,5 @@ enum ReportType: Int {
     case paidVacations
     case unpaidDayOff
     case sickLeave
+    case conference
 }
