9b3fbfa4ea7882231ae77dcc013eb882c27d4666
Bartlomiej Guminiak <guminiak.bartlomiej@gmail.com>
2018-07-02 23:44:42 +0200

    Refactor

--- ELSpace/Commons/Extensions/Double_FromString.swift
+++ ELSpace/Commons/Extensions/Double_FromString.swift
@@ -4,12 +4,20 @@ extension Double {
 
     init?(from string: String?) {
         guard let string = string else { return nil }
+        if let double = Double(fromCommaSeparatedString: string) {
+            self = double
+        } else if let double = Double(string) {
+            self = double
+        } else {
+            return nil
+        }
+    }
+
+    private init?(fromCommaSeparatedString string: String) {
         let formatter = NumberFormatter()
         formatter.decimalSeparator = ","
         if let number = formatter.number(from: string) {
             self = number.doubleValue
-        } else if let double = Double(string) {
-            self = double
         } else {
             return nil
         }
