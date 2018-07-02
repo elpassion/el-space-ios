ca758225966e38c2286ed3d2b24478b9d9785eb8
Bartlomiej Guminiak <guminiak.bartlomiej@gmail.com>
2018-07-02 23:35:49 +0200

    Improve method for converting string to double

--- ELSpace/Commons/Extensions/Double_FromString.swift
+++ ELSpace/Commons/Extensions/Double_FromString.swift
@@ -1,8 +1,18 @@
+import Foundation
+
 extension Double {
 
     init?(from string: String?) {
-        guard let string = string, let double = Double(string) else { return nil }
-        self = double
+        guard let string = string else { return nil }
+        let formatter = NumberFormatter()
+        formatter.decimalSeparator = ","
+        if let number = formatter.number(from: string) {
+            self = number.doubleValue
+        } else if let double = Double(string) {
+            self = double
+        } else {
+            return nil
+        }
     }
 
 }
