93741097eb5ad8fd8d036e972873ac87300a10d0
Bartlomiej Guminiak <guminiak.bartlomiej@gmail.com>
2018-12-14 11:18:18 +0100

    Add ModalViewControllerPresentTransition

---
+++ ELSpace/Commons/Presenters/Modal/ModalViewControllerPresentTransition.swift
@@ -0,0 +1,47 @@
+import UIKit
+import Anchorage
+
+class ModalViewControllerPresentTransition: NSObject, UIViewControllerAnimatedTransitioning {
+
+    init(animator: Animating) {
+        self.animator = animator
+    }
+
+    // MARK: UIViewControllerAnimatedTransitioning
+
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return duration
+    }
+
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        guard let targetView = transitionContext.view(forKey: .to) else { return }
+        transitionContext.containerView.addSubview(targetView)
+        targetView.alpha = 0
+        targetView.frame = transitionContext.containerView.bounds
+        targetView.transform = CGAffineTransform(translationX: 0, y: 200)
+
+        let backgroundView = ModalViewControllerBackgroundView()
+        backgroundView.alpha = 0
+        transitionContext.containerView.insertSubview(backgroundView, belowSubview: targetView)
+        backgroundView.edgeAnchors == transitionContext.containerView.edgeAnchors
+
+        animator.animate(
+            withDuration: transitionDuration(using: transitionContext),
+            animations: {
+                backgroundView.alpha = 1
+                targetView.alpha = 1
+                targetView.transform = .identity
+            },
+            completion: { _ in
+                let didComplete = !transitionContext.transitionWasCancelled
+                transitionContext.completeTransition(didComplete)
+            }
+        )
+    }
+
+    // MARK: Private
+
+    private let duration: TimeInterval = 0.4
+    private let animator: Animating
+
+}
