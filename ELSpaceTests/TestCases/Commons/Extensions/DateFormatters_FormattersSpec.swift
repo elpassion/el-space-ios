d30c0ed4416d8ada5eeca7acaf61f539d566bf06
Bartlomiej <guminiak.bartlomiej@gmail.com>
2018-12-19 16:18:25 +0100

    Add DateFormatters_FormattersSpec

---
+++ ELSpaceTests/TestCases/Commons/Extensions/DateFormatters_FormattersSpec.swift
@@ -0,0 +1,62 @@
+import Quick
+import Nimble
+
+@testable import ELSpace
+
+class DateFormatters_FormattersSpec: QuickSpec {
+
+    override func spec() {
+        describe("DateFormatters_Formatters") {
+            var sut: DateFormatter!
+
+            describe("shortDateFormatter") {
+                beforeEach {
+                    sut = DateFormatter.shortDateFormatter
+                }
+
+                it("should return correct date formatter") {
+                    expect(sut.locale) == Locale(identifier: "en")
+                    expect(sut.timeZone) == TimeZone(identifier: "Europe/Warsaw")
+                    expect(sut.dateFormat) == "yyyy-MM-dd"
+                }
+            }
+
+            describe("dayFormatter") {
+                beforeEach {
+                    sut = DateFormatter.dayFormatter
+                }
+
+                it("should return correct date formatter") {
+                    expect(sut.locale) == Locale(identifier: "en")
+                    expect(sut.timeZone) == TimeZone(identifier: "Europe/Warsaw")
+                    expect(sut.dateFormat) == "d E"
+                }
+            }
+
+            describe("monthFormatter") {
+                beforeEach {
+                    sut = DateFormatter.monthFormatter
+                }
+
+                it("should return correct date formatter") {
+                    expect(sut.locale) == Locale(identifier: "en")
+                    expect(sut.timeZone) == TimeZone(identifier: "Europe/Warsaw")
+                    expect(sut.dateFormat) == "MMMM yyyy"
+                }
+            }
+
+            describe("activityFormatter") {
+                beforeEach {
+                    sut = DateFormatter.activityFormatter
+                }
+
+                it("should return correct date formatter") {
+                    expect(sut.locale) == Locale(identifier: "en")
+                    expect(sut.timeZone) == TimeZone(identifier: "Europe/Warsaw")
+                    expect(sut.dateFormat) == "E, d MMM yyyy"
+                }
+            }
+        }
+    }
+    
+}
