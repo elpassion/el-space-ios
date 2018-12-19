4a031ae3e5f0397317faf99aa38893774bf97359
Bartlomiej <guminiak.bartlomiej@gmail.com>
2018-12-19 17:05:07 +0100

    Add AppContainer_CoordinatorFactoriesSpec

---
+++ ELSpaceTests/TestCases/Containers/AppContainer_CoordinatorFactoriesSpec.swift
@@ -0,0 +1,34 @@
+import Quick
+import Nimble
+
+@testable import ELSpace
+
+class AppContainer_CoordinatorFactoriesSpec: QuickSpec {
+
+    override func spec() {
+        describe("AppContainer_CoordinatorFactories") {
+            var sut: AppContainer!
+
+            beforeEach {
+                sut = AppContainer()
+            }
+
+            it("should build SelectionCoordinator") {
+                expect(sut.selectionCoordinator(googleIdToken: "")).to(beAnInstanceOf(SelectionCoordinator.self))
+            }
+
+            it("should build ActivitiesCoordinator") {
+                expect(sut.activitiesCoordinator()).to(beAnInstanceOf(ActivitiesCoordinator.self))
+            }
+
+            it("should build ActivityCoordinator") {
+                expect(sut.activityCoordinator(activityType: .new(Date()), projectScope: []))
+                    .to(beAnInstanceOf(ActivityCoordinator.self))
+            }
+
+            it("should build ProjectSearchCoordinator") {
+                expect(sut.projectSearchViewModel(projectId: nil)).to(beAnInstanceOf(ProjectSearchCoordinator.self))
+            }
+        }
+    }
+}
